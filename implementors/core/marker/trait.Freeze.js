bb192b6a391cbca456358d7f3f6c2f2da25f700f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-04 11:44:48 +0000

    Deploy slowli/secret-tree to github.com/slowli/secret-tree.git:gh-pages

---
+++ implementors/core/marker/trait.Freeze.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["secret_tree"] = [{text:"impl Freeze for <a class=\"struct\" href=\"secret_tree/struct.SecretTree.html\" title=\"struct secret_tree::SecretTree\">SecretTree</a>",synthetic:true,types:["secret_tree::SecretTree"]},{text:"impl Freeze for <a class=\"struct\" href=\"secret_tree/struct.Name.html\" title=\"struct secret_tree::Name\">Name</a>",synthetic:true,types:["secret_tree::Name"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["secret_tree"] = {"name":"","files":["kdf.rs","lib.rs"]};
+createSourceSidebar();
